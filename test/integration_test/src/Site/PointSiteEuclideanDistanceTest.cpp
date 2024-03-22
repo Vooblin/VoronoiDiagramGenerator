c1c3c6a0c43613734d7d1444a0703b4707d62350
Dmitrii Murygin <dmitrij.murygin7@gmail.com>
2024-03-22 20:17:54 +0600

    Add integration test

---
+++ test/integration_test/src/Site/PointSiteEuclideanDistanceTest.cpp
@@ -0,0 +1,43 @@
+#include <vdg/DistanceFunction/EuclideanDistance.h>
+#include <vdg/Point.h>
+#include <vdg/Site.h>
+#include <vdg/Site/PointSite.h>
+
+#include <gtest/gtest.h>
+
+#include <cmath>
+
+namespace vdg::test {
+
+static_assert(Site<PointSite<Point2D>, Point2D, EuclideanDistance<Point2D>>);
+static_assert(Site<PointSite<Point3D>, Point3D, EuclideanDistance<Point3D>>);
+static_assert(Site<PointSite<PointXD>, PointXD, EuclideanDistance<PointXD>>);
+
+TEST(PointSiteEuclideanDistanceTest, Point2DTest) {
+    const PointSite<Point2D> site(Point2D{1, 2});
+    const Point2D point{3, 4};
+    const EuclideanDistance<Point2D> func;
+    const double expected = std::sqrt(8);
+    const double actual = site.distance(point, func);
+    EXPECT_DOUBLE_EQ(expected, actual);
+}
+
+TEST(PointSiteEuclideanDistanceTest, Point3DTest) {
+    const PointSite<Point3D> site(Point3D{1, 2, 3});
+    const Point3D point{4, 5, 6};
+    const EuclideanDistance<Point3D> func;
+    const double expected = std::sqrt(27);
+    const double actual = site.distance(point, func);
+    EXPECT_DOUBLE_EQ(expected, actual);
+}
+
+TEST(PointSiteEuclideanDistanceTest, PointXDTest) {
+    const PointSite<PointXD> site(PointXD{1, 2, 3, 4});
+    const PointXD point{5, 6, 7, 8};
+    const EuclideanDistance<PointXD> func;
+    const double expected = std::sqrt(64);
+    const double actual = site.distance(point, func);
+    EXPECT_DOUBLE_EQ(expected, actual);
+}
+
+}  // namespace vdg::test
